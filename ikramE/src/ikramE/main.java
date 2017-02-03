395c36b7f5229941b32d8ff562962435cbbb3233
Ikraminfo <anaikra@outlook.fr>
2017-02-03 12:33:42 +0100

    Ajouter comm
    comm1

--- ikramE/src/ikramE/main.java
+++ ikramE/src/ikramE/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("moi melle ikram senoussaoui");
+		System.out.println("moi melle ikram senoussaoui je suis en master 2 informatique spécialité Génie logiciel ........" );
      int a=15;
      System.out.println(a);
      int b=12;
