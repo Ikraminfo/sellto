6d9e3597b4b06660dc086775fe91e5b6a328f922
DELL <DELL@DELL-PC>
2017-02-03 12:15:14 +0100

    initial

---
+++ ikramE/src/ikramE/main.java
@@ -0,0 +1,12 @@
+package ikramE;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("moi melle ikram senoussaoui");
+     int a=15;
+     System.out.println(a);
+	}
+
+}
